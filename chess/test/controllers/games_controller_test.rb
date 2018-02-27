66aa6eeb35de5e9d2133910b25effb2f1662ec08
jay johnson <jayj10080@gmail.com>
2018-02-27 15:15:41 +0000

    changed app/controllers/places_controller.rb, app/views/places, test/controllers/places_controller_test.rb, app/helpers/places_helper.rb, app/assets/javascripts/places.coffee, app/assets/stylesheets/places.scssall to replace places with games. Changed routes.rb to include games

--- chess/test/controllers/games_controller_test.rb
+++ chess/test/controllers/games_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PlacesControllerTest < ActionDispatch::IntegrationTest
+class GamesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
