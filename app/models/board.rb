2ad81b750f208ebb9a0fd92e21b6147c33afb275
Zac Jones <zacjones93@gmail.com>
2018-03-01 13:19:34 -0800

    build_board first pass

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,8 +3,28 @@ class Board < ApplicationRecord
   serialize :board_game, Array
 
   def initialize
-
+    @board_game = []
   end
 
+  def build_board
+    letters = [ "a", "b", "c", "d", "e", "f", "g", "h" ]
+    numbers = [ "1", "2", "3", "4", "5", "6", "7", "8" ]
 
+    # board_game[0]["a1"]
+    # => we want
+    # [ 
+    # {
+    # "a1"=> ""
+    # "b1"=> ""
+    # "c1"=> ""
+    #  ...
+    # "h1" => ""
+    # }  
+    # ]
+    letters.each do |letter|
+      numbers.each do |number|
+        @board_game << {"#{letter}#{number}" => ""}
+      end
+    end
+  end
 end
