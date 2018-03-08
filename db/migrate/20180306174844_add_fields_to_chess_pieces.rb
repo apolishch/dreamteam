36527fe5c6117e24331b71343e9a080a53af7eae
hanshank <hans@hanken.org>
2018-03-09 00:02:01 +0100

    Changed add_fields_to_chess_pieces

--- db/migrate/20180306174844_add_fields_to_chess_pieces.rb
+++ db/migrate/20180306174844_add_fields_to_chess_pieces.rb
@@ -1,9 +1,11 @@
 class AddFieldsToChessPieces < ActiveRecord::Migration[5.0]
   def change
-    add_column :chess_pieces, :game_id, :integer
-    add_column :chess_pieces, :x_position, :integer
-    add_column :chess_pieces, :y_position, :integer
-    add_column :chess_pieces, :checked, :boolean
-    add_column :chess_pieces, :promotable, :boolean
+    create_table :chess_pieces do |t|
+    t.integer :game_id
+    t.integer :x_position
+    t.integer :y_position
+    t.boolean :checked
+    t.boolean :promotable
+    end
   end
 end
