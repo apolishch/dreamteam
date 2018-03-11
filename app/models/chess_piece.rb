f194c4f2878972163465ac8ed2bfc81a8817cb25
hanshank <hans@hanken.org>
2018-03-11 18:19:21 +0100

    New commit to branch

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,2 +1,18 @@
 class ChessPiece < ApplicationRecord
+
+  # Obstructed means that another piece is in the spot the piece is trying to move to
+
+  def is_obstructed?(x, y)
+
+    board = @board
+    target_value = board[x][y]
+
+    # target => where the piece is trying to move
+    if target_value > nil # target is populated with a piece
+      true
+    else
+      false
+    end
+  end
+
 end
