4ff3b3671f04a5b238ff55a453db0a32cb64c8ba
hanshank <hans@hanken.org>
2018-03-14 00:17:34 +0100

    Added tests

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -5,11 +5,11 @@ class ChessPiece < ApplicationRecord
     if (x == self.x_position) && (y == self.y_position) # If the piece is trying to be moved to it's current position
       false
     elsif y == self.y_position # If target is located vertically, meaning on the same column as current position
-      !self.game.pieces.select {|piece| (piece.y_position == y) && piece.x_position.between?(x, x_position)}.empty?
-    elsif (x - self.x_position).abs == (y-self.y_position).abs # If target is located diagonally from starting position
-      !self.game.pieces.select {|piece| ((x - piece.x_position).abs == (y - piece.y_position).abs) && piece.x_position.between?(x, x_position) && piece.y_position.between?(y, y_position)}.empty?
+      self.game.chess_pieces.select {|piece| (piece.y_position == y) && piece.x_position.between?(x, x_position)}.empty?
     elsif x == self.x_position # If target is located horizontally - Same row
-      !self.game.pieces.select {|piece| (piece.x_position == x) && piece.y_position.between?(y, y_position)}.empty?
+      self.game.chess_pieces.select {|piece| (piece.x_position == x) && piece.y_position.between?(y, y_position)}.empty?
+    elsif (x - self.x_position).abs == (y-self.y_position).abs # If target is located diagonally from starting position
+      self.game.chess_pieces.select {|piece| ((x - piece.x_position).abs == (y - piece.y_position).abs) && piece.x_position.between?(x, x_position) && piece.y_position.between?(y, y_position)}.empty?
     else
       false
     end
--- spec/models/chess_piece_spec.rb
+++ spec/models/chess_piece_spec.rb
@@ -2,8 +2,33 @@ require 'rails_helper'
 
 RSpec.describe ChessPiece, type: :model do
 
-  it "Checks if piece is obstructed on a move. Should return true if obstructed and false if not" do
-    
+  context '#is_obstructed?' do
+    let(:game) {FactoryBot.create(:game)}
+    let(:piece) {FactoryBot.create(:chess_piece, game_id: game.id, x_position: 2, y_position: 2)}
+    it 'checks if target is same as current position' do
+      expect(piece.is_obstructed?(2, 2)).to eq(false)
+    end
+
+    it 'checks for horizontal obstruction' do
+      piece2 = FactoryBot.create(:chess_piece, game_id: game.id, x_position: 2, y_position: 3)
+      expect(piece.is_obstructed?(2, 5)).to eq(true)
+    end
+
+    it 'checks for vertical obstruction' do
+      piece2 = FactoryBot.create(:chess_piece, game_id: game.id, x_position: 5, y_position: 2)
+      expect(piece.is_obstructed?(6, 2)).to eq(true)
+    end
+
+    it 'checks for diagonal obstruction' do
+      piece2 = FactoryBot.create(:chess_piece, game_id: game.id, x_position: 3, y_position: 3)
+      expect(piece.is_obstructed?(4, 4)).to eq(true)
+    end
+
+    it 'when piece is not obstructed' do
+      expect(piece.is_obstructed?(0, 0)).to eq(false)
+    end
+
   end
+
 
 end
