0d72e15fce8148997dec08441f099a49f7a4eb8d
Charlie Tyler <charlietyler@live.com>
2018-03-19 17:21:06 +0000

    Updated move rules for rook, bishop, knight, queen

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,2 +1,27 @@
 class Bishop < ChessPiece
+  belongs_to :game
+
+  def x_move_distance(new_x_position) # Finds the distance moved in the x axis
+    x_move_distance = (self.x_position - new_x_position) # .abs stops negatives
+  end
+
+  def y_move_distance(new_y_position) # Finds the distance moved in the y axis
+    y_move_distance = (self.y_position - new_y_position) # .abs stops negatives
+  end
+
+  def diagonal?(x_move_distance, y_move_distance) # Checks that it is a diagonal move from original position
+    x_move_distance == y_move_distance 
+  end
+
+  def valid_move?(new_x_position, new_y_position)
+    if diagonal?(new_x_position, new_y_position)
+      true
+    else
+      false
+    end
+  end
 end
+
+# Return == true || false
+# Bishop can move diagonally from starting position
+# Already checked that piece has moved from starting position
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,2 +1,19 @@
 class Rook < ChessPiece
+  belongs_to :game
+
+  def valid_move?(new_x_position, new_y_position)
+    if (new_x_position == self.x_position && new_y_position != self.y_position)
+      true
+    elsif (new_y_position == self.y_position && new_x_position != self.x_position)
+      true
+    else
+      false
+    end
+  end
 end
+
+# Return == true || false
+# Not working out if it is obstructed, only if it is valid as a principle of chess movements
+
+# Piece must move off from its starting point -- already worked out in chess_piece model
+# Rook can move in either y || x axis but not both in one move
