2f45388fe9bd16c9b922c8c9b868222620ed3479
Hans Martin Hanken <hans@hanken.org>
2018-03-19 13:59:26 +0100

    Revert "Extended join game functionality to avoid joining your own game"

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,13 @@
-class GamesController < ApplicationController
+class GamesController < ApplicationController	
   before_action :authenticate_user!, only: [:show, :new, :create]
 
   def home
   end
 
-  def index
+  def index	
     @games = Game.where.not(:opponent_id => nil)
     @singleplayer = Game.where(:opponent_id => nil)
-  end
+  end	
 
   def show
     @game = Game.find_by_id(params[:id])
@@ -16,24 +16,21 @@ class GamesController < ApplicationController
       render plain: 'Not Found :(', status: :not_found
     end
   end
-
+  
   def update
     @game = Game.find_by_id(params[:id])
-    if current_user.id == @game.user_id
-      flash[:notice] = "Welcome back. Your game is awaiting."
-      redirect_to game_path(@game)
-    elsif @game.opponent_id.nil?
+    if @game.opponent_id.nil?
       @game.update_attributes(opponent_id: current_user.id)
       redirect_to game_path(@game)
     else
       render plain: 'Couldn\'t join the game. Please try another game', status: :unprocessable_entity
     end
   end
-
+    
 
   def new
     @game = Game.new
-
+    
   end
 
   def create
@@ -51,3 +48,5 @@ class GamesController < ApplicationController
     params.require(:game).permit(:game_name, :opponent_id)
   end
 end
+
+
