e0865a95a98e807ce98c839e7fa12a6e5df27c6a
hanshank <hans@hanken.org>
2018-03-26 20:07:53 +0200

    Added en_passant

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,9 +1,37 @@
 class Pawn < ChessPiece
+  belongs_to :game
 
   def valid_direction(color)
-    if (color == white) && (y > self.y_position)
+    if (color == 'white') && (y > self.y_position)
       true
-    elsif (color == black) && (y < self.y_position)
+    elsif (color == 'black') && (y < self.y_position)
+      true
+    else
+      false
+    end
+  end
+
+  def first_move?
+    if (self.color == 'black') && (self.y_position == 1) && (self.promotable == false)
+      true
+    elsif (self.color == 'white') && (self.y_position == 6) && (self.promotable == false)
+      true
+    else
+      false
+    end
+  end
+
+  def diagonal_move(x, y)
+    if (x - self.x_position).abs == 1 && (y - self.y_position).abs == 1
+      true
+    else
+      false
+  end
+
+  def en_passant?(x, y)
+    if (self.color == 'black') && (self.y_position == 4) && (self.diagonal_move(x, y) == true) && (@game[y - 1][self.x_position].color == true) # when moving right behind opponent pawn
+      true
+    elsif (self.color == 'white') && (self.y_position == 3) && (self.diagonal_move(x, y) == true) && (@game[y + 1][self.x_position].color == false) # when moving right behind opponent pawn
       true
     else
       false
@@ -11,23 +39,25 @@ class Pawn < ChessPiece
   end
 
   def valid_move?(x, y)
-    if (x == self.x_position) && (y == self.y_position) # If the piece is trying to be moved to it's current position
+    if (x == self.x_position) && (y == self.y_position) # If trying to move to same position as piece´s current position
       false
-    elsif (y - self.y_position == 1) && (x - self.x_position == 0) # If target is located vertically and the target is 1 tile away
+    elsif self.is_obstructed?(x, y) == true # Pawn can't be obstructed
+      false
+    elsif self.en_passant?(x, y) == true
       true
-    elsif ((x - self.x_position).abs == 1) && ((y - self.y_position).abs == 1) && !(x, y).empty? # If target is located diagonally from start, but only one tile away (left or right)
+    elsif (self.valid_direction(color) == true) # Not allowed to move backwards
       true
-    elsif (self.y_position == 2) && (y - self.y_position == 2) # change y_position to 7 if black pieces => Will allow move of two vertically if standing from starting position.
+    elsif ((y - self.y_position).abs == 1) && (x == self.x_position) # If piece moves 1 tile vertically and is not obstructed
+      true
+    elsif ((x - self.x_position).abs == 1) && ((y - self.y_position).abs == 1) # Diagonal capture where there is a piece present on target destination
+      true
+    elsif (self.y_position == 1) && (self.color == black) && ((y - self.y_position).abs == 2) # If target is 2 tiles away, black piece
+      true
+    elsif ((self.y_position == 6) && (self.color == white)) && ((y - self.y_position).abs == 2) # If target is 2 tiles away, white
       true
     else
       false
     end
   end
 
-
-
-# Can move two tiles vertically when first move
-# Can move 1 tile vertically, up 1 row if white and down 1 row when black
-# Cant' move backwards from where it came.
-# Can't move horizontally
-# Can only capture vertically and 1 tile away
+end
--- spec/factories/chess_pieces.rb
+++ spec/factories/chess_pieces.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :chess_piece do
     association :game
+    association :pawn
+
+  end
+
+  factory :pawn do
+    association :game
+    association :chess_piece
   end
 end
