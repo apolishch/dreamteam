a2bd0591db41aadbf4d506d57d11aab584890d6e
hanshank <hans@hanken.org>
2018-03-26 21:59:47 +0200

    Added moves to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309000329) do
+ActiveRecord::Schema.define(version: 20180326183727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20180309000329) do
     t.datetime "updated_at",  null: false
     t.string   "locations"
     t.integer  "winner_id"
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.integer  "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
