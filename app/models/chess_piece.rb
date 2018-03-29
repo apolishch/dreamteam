dcc7d9c4cdf16d6aa13426c083b23500e0dd3301
hanshank <hans@hanken.org>
2018-03-29 18:16:59 +0200

    Fixed error with is obstructed method

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,32 +1,17 @@
 class ChessPiece < ApplicationRecord
   belongs_to :game
 
-  # def is_obstructed?(x, y)
-  #   if (x == self.x_position) && (y == self.y_position) # If the piece is trying to be moved to it's current position
-  #     false
-  #   elsif y == self.y_position # If target is located horizontally, meaning on the same row as current position
-  #     self.game.chess_pieces.select {|piece| (piece.y_position == y) && piece.x_position.between?(x, x_position)}.empty?
-  #   elsif x == self.x_position # If target is located vertically - Same column
-  #     self.game.chess_pieces.select {|piece| (piece.x_position == x) && piece.y_position.between?(y, y_position)}.empty?
-  #   elsif (x - self.x_position).abs == (y-self.y_position).abs # If target is located diagonally from starting position
-  #     self.game.chess_pieces.select {|piece| ((x - piece.x_position).abs == (y - piece.y_position).abs) && piece.x_position.between?(x, x_position) && piece.y_position.between?(y, y_position)}.empty?
-  #   else
-  #     false
-  #   end
-  # end
-
   def is_obstructed?(x, y)
-    if (x == self.x_position) && (y == self.y_position) # If the piece is trying to be moved to it's current position
-      true
-    elsif (y == self.y_position) && (!self.game.chess_pieces.select {|piece| (piece.y_position == y) && piece.x_position.between?(x, x_position)}.empty?)   # If target is located horizontally, meaning on the same row as current position
-      true
-    elsif (x == self.x_position) && (!self.game.chess_pieces.select {|piece| (piece.x_position == x) && piece.y_position.between?(y, y_position)}.empty?) # If target is located vertically - Same column
-      true
-    elsif ((x - self.x_position).abs == (y - self.y_position).abs) && (!self.game.chess_pieces.select {|piece| ((x - piece.x_position).abs == (y - piece.y_position).abs) && piece.x_position.between?(x, x_position) && piece.y_position.between?(y, y_position)}.empty?) # If target is located diagonally from starting position
-      true
+    if (x == self.x_position) && (y == self.y_position) # Checks if target is same as current position
+      false
+    elsif y == self.y_position # Checks if target is located horizontally from current position
+      !self.game.chess_pieces.select {|piece| (piece.y_position == y) && piece.x_position.between?(x, x_position)}.empty? # Returns true if anye of the tiles in between target and current position are occupied by one or several pieces
+    elsif x == self.x_position # Checks if target is located vertically from current position
+      !self.game.chess_pieces.select {|piece| (piece.x_position == x) && piece.y_position.between?(y, y_position)}.empty? # Returns true if anye of the tiles in between target and current position are occupied by one or several pieces
+    elsif (x - self.x_position).abs == (y-self.y_position).abs # Checks if target is located diagonally from current position
+      !self.game.chess_pieces.select {|piece| ((x - piece.x_position).abs == (y - piece.y_position).abs) && piece.x_position.between?(x, x_position) && piece.y_position.between?(y, y_position)}.empty? # Returns true if anye of the tiles in between target and current position are occupied by one or several pieces
     else
       false
     end
   end
-
 end
