ef585c32cc713d35bd4f40103ffd00ee74e798e0
Jared <mahoneyj02@outlook.com>
2018-03-29 16:12:34 +0000

    fixed tests

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe King, type: :model do
     
     describe '#opponent_pieces' do
         let(:game) {FactoryBot.create(:game)}
-        let(:king_b) {FactoryBot.create(:king, game_id: game.id, x_position: 3, y_position: 2, color: false)}
-        let(:king_w) {FactoryBot.create(:king, game_id: game.id, x_position: 1, y_position: 2, color: true)}
-
+        let!(:queen_b) {FactoryBot.create(:queen, game_id: game.id, x_position: 1, y_position: 5, color: false)}
+        let!(:queen_w) {FactoryBot.create(:queen, game_id: game.id, x_position: 3, y_position: 4, color: true)}
+        let!(:king_b) {FactoryBot.create(:king, game_id: game.id, x_position: 3, y_position: 2, color: false)}
+        let!(:king_w) {FactoryBot.create(:king, game_id: game.id, x_position: 1, y_position: 4, color: true)}
+        let!(:rook_b) {FactoryBot.create(:rook, game_id: game.id, x_position: 1, y_position: 2, color: false)}
+        let!(:rook_w) {FactoryBot.create(:rook, game_id: game.id, x_position: 5, y_position: 3, color: true)}
+        
         
         it 'only returns white pieces when the user\'s pieces are black' do
             opponent_pieces = king_b.opponent_pieces
@@ -25,39 +29,40 @@ RSpec.describe King, type: :model do
         end
         
         it 'only returns the number of pieces the opponent has' do
-            expect(king_b.opponent_pieces.count).to eq(16)
+            expect(king_b.opponent_pieces.count).to eq(3)
         end
     end
     
     describe '#pieces_causing_check' do
         let(:game) {FactoryBot.create(:game)}
-        let!(:queen_b) {FactoryBot.create(:queen, game_id: game.id, x_position: 2, y_position: 3, color: false)}
+        let!(:queen_b) {FactoryBot.create(:queen, game_id: game.id, x_position: 1, y_position: 5, color: false)}
         let!(:queen_w) {FactoryBot.create(:queen, game_id: game.id, x_position: 3, y_position: 4, color: true)}
         let!(:king_b) {FactoryBot.create(:king, game_id: game.id, x_position: 3, y_position: 2, color: false)}
         let!(:king_w) {FactoryBot.create(:king, game_id: game.id, x_position: 1, y_position: 4, color: true)}
         let!(:rook_b) {FactoryBot.create(:rook, game_id: game.id, x_position: 1, y_position: 2, color: false)}
         let!(:rook_w) {FactoryBot.create(:rook, game_id: game.id, x_position: 5, y_position: 3, color: true)}
         
-        xit 'builds an array with only the pieces that cause a king to be in check' do
+        it 'builds an array with only the pieces that cause a king to be in check' do
             expect(king_b.pieces_causing_check.count).to eq(1)
             expect(king_w.pieces_causing_check.count).to eq(2)
+            
         end
     end
     
      describe '#in_check?' do
         let(:game) {FactoryBot.create(:game)}
-        let!(:queen_b) {FactoryBot.create(:queen, game_id: game.id, x_position: 2, y_position: 3, color: false)}
+        let!(:queen_b) {FactoryBot.create(:queen, game_id: game.id, x_position: 1, y_position: 5, color: false)}
         let!(:queen_w) {FactoryBot.create(:queen, game_id: game.id, x_position: 3, y_position: 4, color: true)}
         let!(:king_b) {FactoryBot.create(:king, game_id: game.id, x_position: 3, y_position: 2, color: false)}
         let!(:king_w) {FactoryBot.create(:king, game_id: game.id, x_position: 5, y_position: 4, color: true)}
         let!(:rook_b) {FactoryBot.create(:rook, game_id: game.id, x_position: 1, y_position: 2, color: false)}
         let!(:rook_w) {FactoryBot.create(:rook, game_id: game.id, x_position: 5, y_position: 3, color: true)}
         
-        xit 'returns true if the user\'s king is in check' do
+        it 'returns true if the user\'s king is in check' do
             expect(king_b.in_check?).to eq(true)
         end
         
-        xit 'returns false if a user\'s king is not in check' do
+        it 'returns false if a user\'s king is not in check' do
             expect(king_w.in_check?).to eq(false)
         end
     end
