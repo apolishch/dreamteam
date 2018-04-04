c720904b0313b082e784bda31466d00061f6bada
Jay <jayj10080@gmail.com>
2018-04-04 16:37:36 -0600

    Update king_spec.rb

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -344,38 +344,8 @@ RSpec.describe King, type: :model do
       it 'returns false if a user\'s king is not in check' do
           expect(king_w.in_check?).to eq(false)
       end
-  end
+    end
   
-
-  describe '#valid_move?' do
-    let(:king) { FactoryBot.create(:king) }
-
-    it 'should test if horizontal moves are valid' do
-      expect(king.valid_move?(5, 4)).to eq true
-    end
-
-    it 'should test if vertical moves are valid' do 
-      expect(king.valid_move?(4, 5)).to eq true
-    end
-
-    it 'should test if diagonal moves are valid' do
-      expect(king.valid_move?(5, 5)).to eq true
-    end
-
-    it 'should not allow horizontal movements greater than 1' do
-      expect(king.valid_move?(6, 4)).to eq false
-    end    
-  
-    it 'should not allow vertical movements greater than 1' do
-      expect(king.valid_move?(4, 6)).to eq false
-    end    
-  
-    it 'should not allow diagonal movements greater than 1' do
-      expect(king.valid_move?(6, 6)).to eq false
-    end
-  end
-
-
 end
 
 
