ece7d001e3cbac6862e1bd9d94b65431e9c07ab9
hanshank <hans@hanken.org>
2018-04-05 13:10:51 +0200

    All green tests

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,6 +1,68 @@
 class Pawn < ChessPiece
-    
-    def valid_move?(x, y, color=nil)
-        super
+  belongs_to :game
+
+  def valid_move?(x, y, color=nil)
+      super
+  end
+
+
+  def valid_direction?(x, y)
+    if (self.color == true) && (y < self.y_position)
+      true
+    elsif (self.color == false) && (y > self.y_position)
+      true
+    else
+      false
     end
+  end
+
+  def first_move?(x, y)
+    if (self.color == false) && (self.y_position == 1) && (self.promotable == false) && ((y - self.y_position).abs == 2)
+      true
+    elsif (self.color == true) && (self.y_position == 6) && (self.promotable == false) && ((y - self.y_position).abs == 2)
+      true
+    else
+      false
+    end
+  end
+
+  def diagonal_move(x, y)
+    if (x - self.x_position).abs == 1 && (y - self.y_position).abs == 1
+      true
+    else
+      false
+    end
+  end
+
+  def en_passant?(x, y)
+    if (self.color == false) && (self.y_position == 4) && (self.diagonal_move(x, y) == true) && (@game[y - 1][self.x_position].color == true) # when moving right behind opponent pawn
+      true
+    elsif (self.color == true) && (self.y_position == 3) && (self.diagonal_move(x, y) == true) && (@game[y + 1][self.x_position].color == false) # when moving right behind opponent pawn
+      true
+    else
+      false
+    end
+  end
+
+
+  def valid_move?(x, y)
+    if (x == self.x_position) && (y == self.y_position) # If trying to move to same position as piece´s current position
+      false
+    elsif self.is_obstructed?(x, y) # Pawn can't be obstructed
+      false
+    elsif !self.valid_direction?(x, y) # Not allowed to move backwards
+      false
+    elsif ((y - self.y_position).abs == 1) && (x == self.x_position) # If piece moves 1 tile vertically
+      true
+    elsif self.diagonal_move(x, y) # Diagonal capture where there is a piece present on target destination
+      true
+    elsif (self.valid_direction?(x, y)) && (self.first_move?(x, y))
+      true
+    elsif self.en_passant?(x, y)
+      true
+    else
+      false
+    end
+  end
+
 end
---
+++ spec/models/pawn_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Pawn, type: :model do
+
+  context 'non valid moves' do
+    let(:game) {FactoryBot.create(:game)}
+    let(:black_pawn) {FactoryBot.create(:pawn, color: false, game_id: game.id, x_position: 1, y_position: 1)}
+    let(:white_pawn) {FactoryBot.create(:pawn, color: true, game_id: game.id, x_position: 1, y_position: 6)}
+
+    it "should not allow pawn to move to it's current position" do
+      expect(black_pawn.valid_move?(1, 1)).to eq false
+      expect(white_pawn.valid_move?(1, 6)).to eq false
+    end
+
+    it "should not be valid if piece is obstructed" do
+      FactoryBot.create(:pawn, color: true, game_id: game.id, x_position: 1, y_position: 2)
+
+      expect(black_pawn.valid_move?(1, 1)).to eq false
+      expect(black_pawn.valid_move?(1, 3)).to eq false
+    end
+
+    it "can't move backwards" do
+      white_pawn2 = FactoryBot.create(:pawn, color: true, game_id: game.id, x_position: 1, y_position: 3)
+      black_pawn2 = FactoryBot.create(:pawn, color: false, game_id: game.id, x_position: 2, y_position: 4)
+
+      expect(white_pawn2.valid_move?(1, 4)).to eq false
+      expect(black_pawn2.valid_move?(2, 3)).to eq false
+    end
+  end
+
+  context 'valid moves' do
+    let(:game) {FactoryBot.create(:game)}
+    let(:black_pawn1) {FactoryBot.create(:pawn, color: false, game_id: game.id, x_position: 2, y_position: 2)}
+    let(:white_pawn1) {FactoryBot.create(:pawn, color: true, game_id: game.id, x_position: 2, y_position: 5)}
+
+    it "should be able to move 1 tile vertically"  do
+      expect(black_pawn1.valid_move?(2, 3)).to eq true
+      expect(white_pawn1.valid_move?(2, 4)).to eq true
+    end
+
+    it "should be able to move diagonally if capturing" do
+      expect(black_pawn1.valid_move?(3, 3)).to eq true
+      expect(white_pawn1.valid_move?(3, 4)).to eq true
+    end
+
+    it "can move 2 down if the piece color is black and piece is in starting position" do
+      black_pawn2 = FactoryBot.create(:pawn, color: false, game_id: game.id, x_position: 5, y_position: 1, promotable: false)
+
+      expect(black_pawn2.valid_move?(5, 3)).to eq true
+    end
+
+    it "can move 2 up if the color is white and piece is in starting position" do
+      white_pawn2 = FactoryBot.create(:pawn, color: true, game_id: game.id, x_position: 0, y_position: 6, promotable: false)
+
+      expect(white_pawn2.valid_move?(0, 4)).to eq true
+    end
+  end
+end
