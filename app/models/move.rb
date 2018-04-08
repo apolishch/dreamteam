f4fd7b0d6a65c9c40ccde32fff1df56fdea9d118
Abraham Polishchuk <skilurus@Abrahams-MacBook-Pro.local>
2018-04-08 20:25:55 +0000

    remove dead code

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,11 +1,4 @@
 class Move < ApplicationRecord
   belongs_to :game
   belongs_to :chess_piece
-
-  def update_moves
-    piece = game.chess_pieces.find([game_id])
-    puts piece
-    puts "Cray cray if this is working!"
-  end
-
 end
