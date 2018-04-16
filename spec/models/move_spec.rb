e9ee229469557a8854fe710184ed3810b1fc9c1b
Hans Martin Hanken <hans@hanken.org>
2018-04-16 13:06:22 +0200

    Update move_spec.rb

--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -2,13 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Move, type: :model do
 
-  describe '#valid_move?' do
-    let(:game) {FactoryBot.create(:game)}
-    let(:piece) {FactoryBot.create(:chess_piece, game_id: game.id, x_position: 4, y_position: 4)}
-
-    it "text" do
-
-    end
-  end
-
 end
